381ae237d3ee4b2e5e80541407117412e43b7057
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-04-11 21:21:59 +0700

    Added batchbangun().

--- globalvar.py
+++ globalvar.py
@@ -1,10 +1,14 @@
 # Inisialisasi Global Variable
 # NOTE: BEBERAPA DATA DIBAWAH MASIH ASAL KARENA BLM DILOAD DGN CSV.
 NMaxCandi = 1000
+NMaxJin = 100
 
 users = [
     ["Bondowoso", "cintaroro", "bandung_bondowoso"],
     ["Roro", "Gasukabondo", "roro_jonggrang"],
+    ["jinp1, ngabs", "jin_pembangun"],
+    ["jinp2, ngabs", "jin_pembangun"],
+    ["jinp3, ngabs", "jin_pembangun"],
 ]
 
 candi = [0 for i in range(NMaxCandi)]
@@ -16,6 +20,12 @@ bahan_bangunan = [
 ]
 
 logged_in_user = "Tes"
+
+summoned_jin = [[] for i in range(NMaxJin)]
+summoned_jin[0] = ["jinp1, ngabs", "jin_pembangun"]
+summoned_jin[1] = ["jinp1, ngabs", "jin_pembangun"]
+summoned_jin[2] = ["jinp1, ngabs", "jin_pembangun"]
+# [username, role]
 
 # Load Data
 # load("file/user.csv", users)
