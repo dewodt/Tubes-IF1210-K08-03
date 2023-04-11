922357373510f7c0e719175384427a28f30650a1
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-04-11 17:12:06 +0700

    Fixed data file structure.

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# File: main.py
-# from data import load
-# import commands
+import commands
 
-# Anggap semua fungsi yang dipanggil merupakan fungsi yang sudah dibuat sendiri pada modul lain
-users = []  # Matriks data user
-candi = []  # Matriks data candi
-bahan_bangunan = []  # Data bahan bangunan
-
-# load("file/user.csv", users)
-# load("file/candi.csv", candi)
-# load("file/bahan_bangunan.csv", bahan_bangunan)
-
+# File Utama
 while True:
     masukan = input(">>> ")
-#   commands.run(masukan)
+    # commands.kumpul()
+    # commands.bangun()
