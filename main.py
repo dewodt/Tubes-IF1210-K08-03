bd3a8c6291212a573b7e6edd091e898d800edf7e
Richieleonardo <116273535+Richieleonardo@users.noreply.github.com>
2023-04-14 18:26:18 +0700

    Update main.py
    Update code main.py
    1. Di import file login/logout, help, carirole
    2. Ada tambahan module carirole untuk melengkapi fungsi hf.help()
    3. Adanya variabel yang di set di awal (kondisi_login, username, role)
    4. Penambahan kondisional input user (login, logout, help)

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
 import commands as cmd
 import globalvar as gv
+import loginnlogout as lg
+import help as hf
+import carirole as rf
 
 # File Utama
+kondisi_login = False #set kondisi login awal False
+username = 'None'
+role = ""
+masukan = input(">>> ") 
 while True:
+    if masukan == 'login':
+        if lg.periksa_login(kondisi_login):   
+            print("Login gagal!")
+            print(f"Anda telah login dengan username {username}, silakan lakukan \"logout\" sebelum melakukan login kembali")
+
+        else:
+            user_input = lg.login_input()
+            username = user_input[0]
+            kondisi_login= lg.login_req(user_input)
+            role = rf.find_role(username)
+
+            
+    if masukan == 'help':
+        hf.help(kondisi_login, role)
+             
+    if masukan == 'logout':
+        if lg.periksa_login(kondisi_login):
+            kondisi_login = lg.logout_req()
+
+        else:
+            print("Logout gagal!")
+            print("Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout")
+    
     masukan = input(">>> ")
     # cmd.kumpul()
     # cmd.bangun()
     # cmd.batchbangun()
     # cmd.batchkumpul()
+    # Note : untuk command nya letak di atas loop var masukan 
+    # Note : tiap conditional tidak perlu lagi diberi input,
+        # karena input akan jalan setelah kondisional command selesai
+    # Note : file main.py belum memiliki function exit, 
+        # jadi file masih dalam kondisi infinite loop meminta input user
