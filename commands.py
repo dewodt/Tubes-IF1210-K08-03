4146c084b8536b79b78952fe5bab9fb2ac20376e
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-04-18 14:39:07 +0700

    Fixed help().

--- commands.py
+++ commands.py
@@ -586,7 +586,7 @@ def ayamberkokok():
             print("Selamat, Roro Jonggrang memenangkan permainan!")
             print("*Bandung Bondowoso angry noise*")
             print("Roro Jonggrang dikutuk menjadi candi.")
-        else: # Jika bandung bondowoso menang
+        else:  # Jika bandung bondowoso menang
             print("Yah, Bandung Bondowoso memenangkan permainan!")
 
 
@@ -639,3 +639,53 @@ def save():
     ut.write_csv(folder_save, "candi.csv")
     ut.write_csv(folder_save, "bahan_bangunan.csv")
     print(f"Berhasil menyimpan data di folder save/{folder_save}!")
+
+
+def help():  # (kondisi login, username yang masuk)
+    if gv.logged_in_username != "":
+        if gv.logged_in_role == "bandung_bondowoso":
+            print("=========== HELP ===========")
+            print("1. logout \n Untuk keluar dari akun yang digunakan sekarang")
+            print("2. summonjin \n Memanggil jin dari dunia lain.")
+            print(
+                "Setelah memilih jenis jin yang ingin dipanggil, Bondowoso harus memilih username dan password untuk jin tersebut"
+            )
+            print("3. hapusjin \n menghapus jin dengan memasukkan username jin.")
+            print("Jika jin terhapus, candi yang dibuat oleh jin juga ikut terhapus")
+            print(
+                "4. batchkumpul \n setelah command dijalankan, jin tipe pengumpul akan mengumpulkan bahan secara random"
+            )
+            print(
+                "5. batchbangun \n Setelah command dijalankan setiap jin akan membangun candi dengan bahan yang di random untuk setiap candi"
+            )
+            print(
+                "6. laporanjin \n Mengambil laporan jin untuk mengetahui kinerja para jin"
+            )
+            print(
+                "7. laporancandi \n Mengambil laporan candi mulai dari total candi, ID Candi termahal/termurah, dan jumlah material yang digunakan"
+            )
+
+        elif gv.logged_in_role == "roro_jonggrang":
+            print("=========== HELP ===========")
+            print("1. logout \n Untuk keluar dari akun yang digunakan sekarang")
+            print(
+                "2. hancurkancandi \n Menghancurkan candi sesuai ID candi yang diinput"
+            )
+            print("3. ayamberkokok \n Menyelesaikan permainan")
+
+        elif gv.logged_in_role == "jin_pembangung":
+            print("========== HELP ===========")
+            print("1. logout \n Untuk keluar dari akun yang digunakan sekarang")
+            print("2. bangun \n Jin akan membangun candi jika bahan bangunan cukup")
+
+        elif gv.logged_in_role == "jin_pengumpul":
+            print("========== HELP ===========")
+            print("1. logout \n Untuk keluar dari akun yang digunakan sekarang")
+            print(
+                "2. kumpul \n Jin pengumpul akan mencari bahan berupa pasir, batu, dan air secara random"
+            )
+
+    else:
+        print("=========== HELP ===========")
+        print("1. logout \n Untuk keluar dari akun yang digunakan sekarang")
+        print("2. exit \n Untuk keluar dari sistem program")
